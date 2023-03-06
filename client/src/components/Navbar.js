f5043efa266a5bae118c15f108c057610d13ef0b
ifandraliuk <fandraliuk.io@gmail.com>
2023-03-06 13:08:33 +0100

    companions route added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -34,7 +34,7 @@ function NavbarComp() {
                 <Nav.Link as={Link} to="/inventory"><FontAwesomeIcon icon={faBoxOpen} /> Inventar</Nav.Link>
                 <Nav.Link as={Link} to="/talents"><FontAwesomeIcon icon={faDiceD20} /> Talente</Nav.Link>
                 <Nav.Link as={Link} to="/bestiaria"><FontAwesomeIcon icon={faPaw} /> Bestiarium</Nav.Link>
-                <Nav.Link as={Link} to="/bestiaria"><FontAwesomeIcon icon={faUserPlus} /> Begleiter</Nav.Link>
+                <Nav.Link as={Link} to="/companions"><FontAwesomeIcon icon={faUserPlus} /> Begleiter</Nav.Link>
               </Nav>
               <Nav>
               <Nav.Link>Datenbank</Nav.Link>
