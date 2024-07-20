ad70b1f9e20cccfe5c94845c53e6cd973855b9cd
ifandraliuk <fandraliuk.io@gmail.com>
2024-07-20 09:52:19 +0200

    create functionality fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 //import './App.css';
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import "./Styles/Main.css";
 //import 'react-toastify/dist/ReactTostify.css'
@@ -11,18 +11,27 @@ import Talents from "./pages/Talents/index";
 import Bestiaria from "./pages/Bestiarium/index";
 import Enemies from "./pages/Enemies";
 import InventoryPage from "./pages/Inventory/index";
-import {useSelector } from "react-redux";
+import {useSelector, useDispatch } from "react-redux";
 import Diary from "./pages/Diary/index";
 import Companions from './pages/Companions/index';
 import Dashboard from "./pages/Dashboard/index";
 import { AnimatePresence } from "framer-motion";
 import Navbar from "./components/Nav";
 import Atlas from "./pages/Atlas";
+import { getFraction } from "./features/player/playerSlice";
 
 function App() {
   const location = useLocation();
+  const dispatch = useDispatch()
   const { fractionTheme} =
     useSelector((state) => state.player);
+console.log(fractionTheme.length)
+  useEffect(()=>{
+    if(localStorage.getItem("user") && fractionTheme?.length === 0 ){
+      console.log("fraction theme loading...")
+      dispatch(getFraction())
+    }
+  },[dispatch, fractionTheme])
   
   return (
     <div className="g-0">
@@ -32,7 +41,7 @@ function App() {
         <Routes location={location} key={location.pathname}>
           <Route index element={<LoginPage />}></Route>
           <Route path="/" element={<LoginPage />}></Route>
-          <Route path="/register" element={<CreateCharacter />}></Route>
+          <Route path="/create" element={<CreateCharacter />}></Route>
           <Route exact path="/diary" element={<Diary />}></Route>
           <Route exact path="/player" element={<Dashboard />}></Route>
           <Route exact path="/inventory" element={<InventoryPage />}></Route>
